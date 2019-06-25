16a633c7a47e4c27556a70a54d3645a2916871c1
skennel2 <skennel2@gmail.com>
2019-06-25 22:07:06 +0900

    Source cleanup

---
+++ demo/src/main/java/com/example/demo/controller/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+
+public class AccountNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public AccountNotFoundException() {
+	}
+}
--- demo/src/main/java/com/example/demo/controller/AccountRestController.java
+++ demo/src/main/java/com/example/demo/controller/AccountRestController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Account;
@@ -16,12 +21,32 @@ import com.example.demo.repository.AccountRepositiry;
 public class AccountRestController {
 	
 	@Autowired
-	private AccountRepositiry accountRepositiry;
+	private AccountRepositiry accountRepo;
+	
+	@RequestMapping(method = RequestMethod.OPTIONS)
+	public ResponseEntity<?> options(){
+		return ResponseEntity
+				.ok()
+				.allow(HttpMethod.GET, HttpMethod.DELETE, HttpMethod.OPTIONS, HttpMethod.POST)
+				.build();
+	}
 	
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<Account> getByLoginId(@PathVariable Long id){
-		return accountRepositiry.findById(id)
-				.map(account -> new ResponseEntity<>(account, HttpStatus.OK))
+		return accountRepo.findById(id)
+				.map(account -> ResponseEntity.ok(account))
 				.orElseThrow(() ->new RuntimeException());		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Account>> getAll(){
+		return ResponseEntity.ok(accountRepo.findAll());
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		return accountRepo.findById(id)
+				.map(exists -> ResponseEntity.noContent().build())
+				.orElseThrow(()-> new AccountNotFoundException());
 	}
 }
