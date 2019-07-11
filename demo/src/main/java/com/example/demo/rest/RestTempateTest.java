cfae6981ade47f580bcc5f49c6356fd97b4e5fa7
skennel2 <skennel2@gmail.com>
2019-07-11 10:09:33 +0900

    RestTemplate

--- demo/src/main/java/com/example/demo/rest/RestTempateTest.java
+++ demo/src/main/java/com/example/demo/rest/RestTempateTest.java
@@ -1,9 +1,13 @@
 package com.example.demo.rest;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * http://dummy.restapiexample.com/
- * 해당 사이트에서 Rest Api제공받음
+ * http://dummy.restapiexample.com/ 해당 사이트에서 Rest Api제공받음
  * 
  * @author skennel
  *
@@ -24,31 +27,51 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class RestTempateTest {
 
 	@GetMapping(path = "/employee/{id}")
-	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) throws IOException{
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) throws IOException {
 		RestTemplate restTemplate = new RestTemplateBuilder()
-				.defaultMessageConverters()
-				.rootUri("http://dummy.restapiexample.com/api/v1/")				
+				.rootUri("http://dummy.restapiexample.com/api/v1/")
 				.build();
-		
-		// 해당 api의 accept header가 html타입이라 일단 String으로 받음 
-		String data = restTemplate.getForObject("/employee/{id}", String.class, id);
-		
-		Employee emp =  new ObjectMapper()
-				.readerFor(Employee.class)
-				.readValue(data);
-		
-		return ResponseEntity.ok(emp);
+
+		// 해당 api의 accept header가 text/html타입이라 일단 String으로 받음
+		String jsonData = restTemplate.getForObject("/employee/{id}", String.class, id);
+		if (StringUtils.hasText(jsonData) && !jsonData.equals("false")) {
+			Employee emp = new ObjectMapper()
+					.readerFor(Employee.class)
+					.readValue(jsonData);
+
+			return ResponseEntity.ok(emp);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
-	@GetMapping(path = "/self/employee/{id}")
-	public ResponseEntity<Employee> selfCall(@PathVariable Long id) throws IOException{
+	@GetMapping(path = "/exchange/employee/{id}")
+	public ResponseEntity<Employee> getEmployeeByIdUsingExchange(@PathVariable Long id) throws IOException {
 		RestTemplate restTemplate = new RestTemplateBuilder()
-				.defaultMessageConverters()
-				.rootUri("http://localhost:8080/rest/api/")				
+				.rootUri("http://dummy.restapiexample.com/api/v1/")
 				.build();
 		
-		// 해당 api의 accept header가 application/json 바로 Serialize 가능함 
-		Employee data = restTemplate.getForObject("/employee/{id}", Employee.class, id);		
+		ResponseEntity<String> response = restTemplate.exchange("/employee/{id}", HttpMethod.GET, null, String.class , id);
+		
+		if(response.getStatusCode().equals(HttpStatus.OK)) {
+			Employee emp = new ObjectMapper()
+					.readerFor(Employee.class)
+					.readValue(response.getBody());
+			
+			return ResponseEntity.ok(emp); 
+		}
+				
+		return ResponseEntity.notFound().build();
+	}
+
+	@GetMapping(path = "/self/employee/{id}")
+	public ResponseEntity<Employee> selfCall(@PathVariable Long id) throws IOException {
+		RestTemplate restTemplate = new RestTemplateBuilder()
+				.rootUri("http://localhost:8080/rest/api/")
+				.build();
+
+		// 해당 api의 accept header가 application/json 바로 Serialize 가능함
+		Employee data = restTemplate.getForObject("/employee/{id}", Employee.class, id);
 		return ResponseEntity.ok(data);
 	}
 }
