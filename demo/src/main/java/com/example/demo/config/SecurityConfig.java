c207ff2ad73514f3a03999dceac415f9d7b899ea
skennel2 <skennel2@gmail.com>
2019-07-12 10:36:32 +0900

    임시로 csrf disable처리, postman으로 post가 동작안해서 임시방편

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.example.demo.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * 참고자료
+ * https://www.popit.kr/spring-security-ajax-%ED%98%B8%EC%B6%9C-%EC%8B%9C-csrf-%EA%B4%80%EB%A0%A8-403-forbidden-%EC%97%90%EB%9F%AC/
+ * @author skennel
+ *
+ */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/account/**").permitAll()
-				.antMatchers("/article/**").authenticated()
+		 		.antMatchers(HttpMethod.POST, "/api/account/**").permitAll()
+		 		.antMatchers("/api/account/**").permitAll()
 				.and()
-			.csrf();						
+			.csrf()
+				.disable();						
 	}	
 	
 	@Override
