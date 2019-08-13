071982392129967da92796819562f914eb59158e
skennel2 <skennel2@gmail.com>
2019-08-13 17:35:37 +0900

    버그수정

--- demo/src/main/java/com/example/demo/config/AuthServerConfig.java
+++ demo/src/main/java/com/example/demo/config/AuthServerConfig.java
@@ -3,12 +3,12 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
@@ -26,13 +26,16 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	private TokenStore tokenStore;
 	
 	private PasswordEncoder passwordEncoder;
+	
+	private UserDetailsService userDetailsService;  
 			
 	@Autowired
 	public AuthServerConfig(AuthenticationManager authenticationManager, TokenStore tokenStore,
-			PasswordEncoder passwordEncoder) {
+			PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
 		this.authenticationManager = authenticationManager;
 		this.tokenStore = tokenStore;
 		this.passwordEncoder = passwordEncoder;
+		this.userDetailsService = userDetailsService;
 	}
 
 	/**
@@ -54,12 +57,8 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 			.authenticationManager(authenticationManager)
-			.tokenStore(tokenStore);
-	}
-	
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.checkTokenAccess("isAuthenticated()");
+			.userDetailsService(userDetailsService)
+			.tokenStore(tokenStore);		
 	}
 	
 }
